fdb2ce6b58648cfa81a8b1ed104a73941523494e
Superbil <superbil@gmail.com>
2015-12-30 05:45:17 +0800

    重構一下

--- shp2motc.py
+++ shp2motc.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Superbil'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 import sys
 import logging
@@ -21,6 +21,42 @@ def create_mapping(fields):
     log.debug(mapping)
     return mapping
 
+def setElement(sf, mapping, root):
+    # net:ReferedProperty
+    referedProperty = ET.SubElement(root, 'net:ReferedProperty')
+    rID = ET.SubElement(referedProperty, 'ro:RoadIdentity')
+
+    n = ET.SubElement(rID, "ro:roadName")
+    n.text = str(r[m['roadname']])
+
+    # rc = ET.SubElement(rID, "ro:roadCode")
+    # rc.text = str(r[m['roadcode']])
+
+    roadType = ET.SubElement(rID, "ro:roadClass")
+    roadType.text = str(r[m['roadtype']])
+
+    # roadaliasn = ET.SubElement(rID, "ro:roadAliasName")
+    # roadaliasn.text = str(r[m['roadaliasn']])
+
+    link = ET.SubElement(root, 'net:link')
+    link.set('xlink:type', 'simple')
+    rlink = ET.SubElement(link, 'ro:RoadLink')
+
+    nGeo = ET.SubElement(rlink, 'net:geometry')
+    nGeo.set('xlink:type', 'simple')
+
+    trival = ET.SubElement(rlink, 'ro:trival')
+    trival.text = 'false'
+
+    rStruct = ET.SubElement(rlink, 'ro:structureType')
+    rStruct.text = r[m['roadstruct']]
+
+    rLinkID = ET.SubElement(rlink, 'ro:roadLinkID')
+    rLinkID.text = "{}-{}".format(r[m['roadid']], r[m['roadcomnum']])
+
+    rID = ET.SubElement(root, 'ro:roadID')
+    rID.text = r[m['roadid']]
+
 log = logging.getLogger()
 # log.setLevel(logging.DEBUG)
 sf = shapefile.Reader("shapefiles/路網數值圖103年_西屯區道路路段")
@@ -33,41 +69,7 @@ if sf.shapeType is 3:
     log.debug(m)
 
     for r in sf.records():
-        # net:ReferedProperty
-        referedProperty = ET.SubElement(root, 'net:ReferedProperty')
-        rID = ET.SubElement(referedProperty, 'ro:RoadIdentity')
-
-        n = ET.SubElement(rID, "ro:roadName")
-        n.text = str(r[m['roadname']])
-
-        # rc = ET.SubElement(rID, "ro:roadCode")
-        # rc.text = str(r[m['roadcode']])
-
-        roadType = ET.SubElement(rID, "ro:roadClass")
-        roadType.text = str(r[m['roadtype']])
-
-        # roadaliasn = ET.SubElement(rID, "ro:roadAliasName")
-        # roadaliasn.text = str(r[m['roadaliasn']])
-
-        link = ET.SubElement(root, 'net:link')
-        link.set('xlink:type', 'simple')
-        rlink = ET.SubElement(link, 'ro:RoadLink')
-
-        nGeo = ET.SubElement(rlink, 'net:geometry')
-        nGeo.set('xlink:type', 'simple')
-
-        trival = ET.SubElement(rlink, 'ro:trival')
-        trival.text = 'false'
-
-        rStruct = ET.SubElement(rlink, 'ro:structureType')
-        rStruct.text = r[m['roadstruct']]
-
-        rLinkID = ET.SubElement(rlink, 'ro:roadLinkID')
-        rLinkID.text = "{}-{}".format(r[m['roadid']], r[m['roadcomnum']])
-
-        rID = ET.SubElement(root, 'ro:roadID')
-        rID.text = r[m['roadid']]
-
+        setElement(sf, m, root)
         break
 
     log.debug(ET.dump(root))
